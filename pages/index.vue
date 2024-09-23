9a81036305927290027ddb4d171a21689669bc63
akademik0305 <tuychiyevakademik@gmail.com>
2024-09-23 13:32:13 +0500

    added cars

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+useSeoMeta({
+	title: 'Home | CRM System',
+})
+</script>
 <template>
 	<div class="p-10">
 		<h2 class="font-bold text-2xl mb-10">CRM System by Red Group</h2>
+		<div>
+			<UiCard class="mb-3" draggable="true">
+				<UiCardHeader role="button">name card</UiCardHeader>
+				<UiCardContent>Kompaniya</UiCardContent>
+				<UiCardFooter>Date</UiCardFooter>
+			</UiCard>
+		</div>
 	</div>
 </template>
 <style></style>
